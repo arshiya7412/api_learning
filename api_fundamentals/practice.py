b277cafdef58e14f8f7b52c5c8bfd16bb05c8fa1
Arshiya Sana <arshiyasana2006@gmail.com>
2026-09-10 09:22:36 +0530

    add api query parameter

--- api_fundamentals/practice.py
+++ api_fundamentals/practice.py
@@ -7,3 +7,7 @@ print(response.status_code)
 names = response.json()
 for na_me in names:
     print(na_me['name'])
+response = requests.get("https://jsonplaceholder.typicode.com/users", params = {"username": "Bret"})
+print(response)
+data = response.json()
+print(data[0]['name'])
